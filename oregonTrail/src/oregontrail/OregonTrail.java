4365eebcd64a525bfb475b4bf7376c09049ad2fe
Cece's <Cece's@Ceces-Outlet.Home>
2017-10-06 18:52:18 -0700

    Updated Test code for New classes added

--- oregonTrail/src/oregontrail/OregonTrail.java
+++ oregonTrail/src/oregontrail/OregonTrail.java
@@ -7,6 +7,15 @@ package oregontrail;
 
 import byui.cit260.oregonTrail.model.Item;
 import byui.cit260.oregonTrail.model.Player;
+import byui.cit260.oregonTrail.model.DailyTrailScene;
+import byui.cit260.oregonTrail.model.FortScene;
+import byui.cit260.oregonTrail.model.GatheringScene;
+import byui.cit260.oregonTrail.model.HotelScene;
+import byui.cit260.oregonTrail.model.HuntingScene;
+import byui.cit260.oregonTrail.model.RiverScene;
+import byui.cit260.oregonTrail.model.StoreScene;
+import byui.cit260.oregonTrail.model.TownScene;
+
 
 /**
  *
@@ -40,6 +49,51 @@ public class OregonTrail {
         int ammoWeight = Item.Ammo.getWeight();
         
         System.out.println(Item.Ammo.toString());
+        
+  
+        //        Testing DailyTrailScene
+        DailyTrailScene newTrail = new DailyTrailScene ();
+        newTrail.setCurrentPlantPresence(true);
+        newTrail.setCurrentAnimalPresence(false);
+        newTrail.setCurrentHazards(true);
+        newTrail.setCurrentDate("Oct 6th 2017");
+        newTrail.setCurrentWeather("Sunny");
+        
+        Boolean curPlants = newTrail.getCurrentPlantPresence();
+        Boolean curAnimals = newTrail.getCurrentAnimalPresence();
+        Boolean curHazards = newTrail.getCurrentHazards();
+        String curDate = newTrail.getCurrentDate();
+        String curWeather = newTrail.getCurrentWeather();
+        System.out.println(newTrail.toString());
+        
+        //        Testing FortScene
+        FortScene newFort = new FortScene ();
+        System.out.println(newFort.toString());
+        
+        //        Testing RiverScene
+        RiverScene newRiver = new RiverScene();
+        newRiver.setRiverDepth(20);
+        int curRiverDepth = newRiver.getRiverDepth();
+        System.out.println(newRiver.toString());
+        //        Testing FortScene
+        TownScene newTown = new TownScene ();
+        System.out.println(newTown.toString());
+        
+        //        Testing HotelScene
+        HotelScene newHotel = new HotelScene ();
+        System.out.println(newHotel.toString());
+       
+        //        Testing StoreScene
+        StoreScene newStore = new StoreScene ();
+        System.out.println(newStore.toString());
+        
+        //        Testing HuntingScene
+        HuntingScene newHunt = new HuntingScene ();
+        System.out.println(newHunt.toString());
+        
+        //        Testing GatheringScene
+        GatheringScene newGat = new GatheringScene ();
+        System.out.println(newGat.toString());
     }
     
 }
