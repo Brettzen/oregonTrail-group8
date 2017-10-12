447b4ff4a0d8cc7dbfa781a0dde48edad798f03c
Cece's <Cece's@Ceces-Outlet.isagenixcorp.com>
2017-10-12 10:25:37 -0700

    New Scene class added

---
+++ oregonTrail/src/byui/cit260/oregonTrail/model/Scene.java
@@ -0,0 +1,72 @@
+/*
+
+ */
+package byui.cit260.oregonTrail.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author J.R. Basham
+ */
+public class Scene implements Serializable{
+    private Location location;
+    private String description;
+    
+    public Scene () {
+    
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.location);
+        hash = 97 * hash + Objects.hashCode(this.description);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Scene other = (Scene) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.location, other.location)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Scene{" + "location=" + location + ", description=" + description + '}';
+    }
+    
+    
+}
