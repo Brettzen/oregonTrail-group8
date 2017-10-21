6695eef329ddd62aa1678049cbc0b3711f18967c
Cece's <Cece's@Ceces-Outlet.isagenixcorp.com>
2017-10-21 12:06:53 -0700

    Added Scene Class to FortScene

--- oregonTrail/src/byui/cit260/oregonTrail/model/FortScene.java
+++ oregonTrail/src/byui/cit260/oregonTrail/model/FortScene.java
@@ -12,12 +12,59 @@ import java.util.Objects;
 public class FortScene implements Serializable{
     private Scene scene;
     private StoreScene storeScene;
+    
     public FortScene() {
         
     }
 
     public Scene getScene() {
         return scene;
+    }
+
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public StoreScene getStoreScene() {
+        return storeScene;
+    }
+
+    public void setStoreScene(StoreScene storeScene) {
+        this.storeScene = storeScene;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.scene);
+        hash = 71 * hash + Objects.hashCode(this.storeScene);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FortScene other = (FortScene) obj;
+        if (!Objects.equals(this.scene, other.scene)) {
+            return false;
+        }
+        if (!Objects.equals(this.storeScene, other.storeScene)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "FortScene{" + "scene=" + scene + ", storeScene=" + storeScene + '}';
     }
 
   
