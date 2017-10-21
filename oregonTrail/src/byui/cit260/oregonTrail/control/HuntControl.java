35d743563212de587bc254c57f2c5e3ea4b65162
Cece's <Cece's@Ceces-Outlet.Home>
2017-10-21 12:04:14 -0700

    Added calcHuntSccess Function to HuntControl class

--- oregonTrail/src/byui/cit260/oregonTrail/control/HuntControl.java
+++ oregonTrail/src/byui/cit260/oregonTrail/control/HuntControl.java
@@ -1,25 +1,29 @@
 /*
- * Copyright (C) 2017 Brett Starks
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  */
 package byui.cit260.oregonTrail.control;
 
 /**
  *
- * @author Brett Starks
+ * @author J.R. Basham
  */
 public class HuntControl {
+    
+    public int calcHuntSuccess(int charHuntingSkill, int wildLifeAmount, float charStamina) {
+        float amountFood;
+        if ((charHuntingSkill <= 0) || (charHuntingSkill > 10)){ 
+               return -1;
+        } else if (wildLifeAmount < 10) {
+               return -1;
+        } else if ((charStamina <= 0) || (charStamina > 1)){
+               return -1;
+        } else {
+            float newWildLifeAmount = wildLifeAmount * charStamina;  
+            newWildLifeAmount = newWildLifeAmount / charHuntingSkill;
+            amountFood = newWildLifeAmount * 5;
+            amountFood = Math.round(amountFood);
+        }
+        return (int) amountFood;
+    }
     
 }
